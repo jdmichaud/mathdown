2212bb3e3cd0279a9022f50d0477d8fb9eaa95a6
jd <jean.daniel.michaud@gmail.com>
2018-05-27 09:56:22 +0200

    Add diagrams: 1. Include latex2js code 2. Disable backend rendering and replace it with frontend rendering

--- src/backend.js
+++ src/backend.js
@@ -13,25 +13,37 @@ const Backend = function Backend(config) {
 
   this.routes = {
     gets: {
+      // Because latex2js does not work without a DOM, rendering in the backend
+      // is not possible anymore.
+      // '/toHtml': async (req, res, next) => {
+      //   if (!lodash.has(req.query, 'url')) {
+      //     res.status(400);
+      //     next();
+      //   }
+      //   const response = await fetch(req.query.url);
+      //   try {
+      //     const html = await toHtml(response.text());
+      //     const page = Mustache.render(template, {
+      //       title: req.query.url,
+      //       html,
+      //     });
+      //     res.send(page);
+      //   } catch (e) {
+      //     console.log(e);
+      //     if (e.name === 'ParseError') {
+      //       res.send(e.message);
+      //     }
+      //   }
+      // },
+      // Full page render in the frontend because of latex2js
       '/toHtml': async (req, res, next) => {
         if (!lodash.has(req.query, 'url')) {
           res.status(400);
           next();
         }
-        const response = await fetch(req.query.url);
-        try {
-          const html = await toHtml(response.text());
-          const page = Mustache.render(template, {
-            title: req.query.url,
-            html,
-          });
-          res.send(page);
-        } catch (e) {
-          console.log(e);
-          if (e.name === 'ParseError') {
-            res.send(e.message);
-          }
-        }
+        res.sendFile('tohtml.html', {
+          root: config.static_path,
+        });
       },
       '/[0-9a-z]{32}': async (req, res) => {
         res.sendFile('index.html', {
